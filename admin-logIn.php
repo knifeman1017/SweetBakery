3351ac8e29d9588cc9ee7f0948484538aa2ed839
DuongCa0 <47597043+DuongCa0@users.noreply.github.com>
2019-09-03 15:40:52 +0700

    bổ sung

---
+++ admin-logIn.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+       <!-- SweetsBakery. logo-->
+        <link rel="icon" href="../SweetBakery/images/logo.jpg">
+        <title>Login Admin</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+        <link href="css/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body style="background: url(images/logo.jpg);">
+        <div class="container w-50 border border-primary" style="border-radius: 25px;margin-top: 10%; background: white;">
+            <h1 class="mt-5" align="center">LOGIN ADMIN</h1>
+            <hr/>        
+            <form method="get">
+                <div class="form-group row text-right">
+                    <label for="user" class="font-weight-bold col-sm-3 col-form-label ">User</label>
+                    <div class="col-sm-9">
+                        <input type="text" id="user" name="user" class="form-control w-75" placeholder="Enter User" required />
+                        
+                    </div>
+                </div>
+                <div class="form-group row text-right">
+                    <label for="pass" class="font-weight-bold col-sm-3 col-form-label">Password</label>
+                    <div class="col-sm-9">
+                        <input type="password" id="pass" name="pass" class="form-control w-75" placeholder="Enter Password">
+                    </div>
+                </div>
+                <div class="form-group mb-5" align="center">
+                    <input class="form-control-sm btn btn-primary" type="submit" value="Login" name="btLogin" />
+                    <input type="reset" class="form-control-sm btn btn-info" value="Reset">
+                </div>
+            </form>
+           
+        </div>
+    </body>
+</html>
+<?php
+
+//kiem tra nut submit da duoc bam chua
+if (isset($_GET["btLogin"]) == TRUE) {
+    // lay du lieu trong 2 o username, password
+    $user = $_GET["user"];
+    $password = $_GET["pass"];
+
+    // tao ket noi voi db
+    include_once './lib/connect.inc';
+
+    //tao linh truy van
+    $sql = "select * from tb_customer where customer_id = '$user'";
+
+    //thuc hien hieu lenh truy van
+    $result = mysqli_query($link, $sql);
+
+    //kiem tra loi
+    if (mysqli_connect_errno($link)) {
+        // neu co loi sai => thong bao loi
+        mysqli_error($link);
+        exit();
+    }
+    if (mysqli_num_rows($result) == 0) {
+        die('<h3> tai khoan ko ton tai, vui long nhap lai !!!</h3>');
+    }
+    // da tim thay dong co ma tk == user, tiep tuc kiem tra password
+    //doc dong du lieu 
+    $row = mysqli_fetch_row($result);
+    if ($row[1] == $password) {
+        // tai khoan va mat khau hop le => chuyen trang 
+        if ($row[2] == 1) {
+            header("Location: admin-Index.php");
+        }
+       
+       
+    }
+     else {
+            die("<h3>mat ma ko dunh vui long nhap lai</h3>");
+        }
+}
+?>      
--- editAcc.php
+++ editAcc.php
@@ -1,11 +1,10 @@
-
 <!DOCTYPE html>
 <html>
     <head>
-        <title>SweetsBakery - SignUp</title>
+        <title>SweetsBakery - EditAcc</title>
 
         <!-- SweetsBakery. logo-->
-        <link rel="icon" href="../images/logo.jpg">
+        <link rel="icon" href="../SweetBakery/images/logo.jpg">
 
 
         <!--CSS FRAMEWORK-->
@@ -14,27 +13,25 @@
         <link href = "//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-glyphicons.css" rel = "stylesheet">
         <link href="css/animate.css" rel='stylesheet' type='text/css' />
         
+        
        
         <link href="css/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
-        
-        <!--Prevent Jquery conflict -->
+
         <script>var $j = jQuery.noConflict(true);</script>
-          
     </head>
     <body>
-        
-         <!-- Header-->
+        <!-- Header-->
         <div class="header" style="background-color: #F4F4F4">
             <div class="container-fluid" style="height: 100px;">
                 <div class=" row top-header">
                     <div class="col-sm-2" style="left: 100px;" >
-                        <a href="homepage.php"><img width="100"  height=" 100" src="../images/logo.jpg"  alt="logo" /></a>
+                        <a href="homepage.php"><img width="100"  height=" 100" src="../SweetBakery/images/logo.jpg"  alt="logo" /></a>
                     </div>
                     <div class="col-sm-8" style="text-align: center;color: #FF5B35;font-weight: bold;top: 40px;font-size: 1.2em;">
                         <p>Welcome SweetsBakery</p>
                         <p>VIỆT NAM</p>
                     </div>
-                    
+
                 </div>
             </div>
         </div>
@@ -46,91 +43,95 @@
                     <div class="container-fluid" >
                         <ul class="nav navbar-nav">
                             <li ><a href="homepage.php" style="color: #FF5B35"><i class="glyphicon glyphicon-home"></i></a></li>
-                            
-                            <li><a  style="color: #FF5B35" href="product.php?cat=banhmi">Bánh mì</a></li>
-                            <li><a  style="color: #FF5B35" href="product.php?cat=banhkem">Bánh kem</a></li>
-                            <li><a  style="color: #FF5B35" href="product.php?cat=banhngot">Bánh ngọt</a></li>
-                            <li><a  style="color: #FF5B35" href="product.php?cat=donggoi">Đóng gói</a></li>
-                            <li><a  style="color: #FF5B35" href="product.php?cat=theomua">Theo mùa</a></li>
-                            <li><a  style="color: #FF5B35" href="product.php?cat=khac">Khác</a></li>
+                                                      
+                            <li><a style="color: #FF5B35" href="product.php?cat=banhmi">Bánh mì</a></li>
+                            <li><a style="color: #FF5B35" href="product.php?cat=banhkem">Bánh kem</a></li>
+                            <li><a style="color: #FF5B35" href="product.php?cat=banhngot">Bánh ngọt</a></li>
+                            <li><a style="color: #FF5B35" href="product.php?cat=donggoi">Đóng gói</a></li>
+                            <li><a style="color: #FF5B35" href="product.php?cat=theomua">Theo mùa</a></li>
+                            <li><a style="color: #FF5B35" href="product.php?cat=khac">Khác</a></li>
                             <li><a style="color: #FF5B35" href="viewStores.php">Cửa hàng</a></li>
                             <li><a style="color: #FF5B35" href="aboutus.php">Về chúng tôi </a></li>
 
                         </ul>
-                        
+
                         <ul class="nav navbar-nav navbar-right">
                             <li>
                                 <a href="cart.php"><button class="btn btn-success btn-lg" type="button" style="background-color: #FF5B35; padding: 5px;">
-                                    <i class="glyphicon glyphicon-shopping-cart"></i> <span class="badge"> 
-                                        <?php
-                                        if (isset($_SESSION["totalQty"])) {
-                                            echo $_SESSION["totalQty"];
-                                        } else {
-                                            echo 0;
-                                        }
-                                        ?></span>
-                                </button></a>
+                                        <i class="glyphicon glyphicon-shopping-cart"></i> <span class="badge"> 
+                                            <?php
+                                            if (isset($_SESSION["totalQty"])) {
+                                                echo $_SESSION["totalQty"];
+                                            } else {
+                                                echo 0;
+                                            }
+                                            ?></span>
+                                    </button></a>
                             </li>
                         </ul>
-                        
+
                         <ul class="nav navbar-nav navbar-right">
-                            <li><a href="#"   style="color: #FF5B35"><span class="glyphicon glyphicon-user" ></span> Sign Up</a></li>
+                            <li><a href="SignUp.php"   style="color: #FF5B35"><span class="glyphicon glyphicon-user" ></span> Sign Up</a></li>
                             <li><a href="Login.php"  style="color: #FF5B35"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
                         </ul>
                     </div>
                 </nav>
             </div>
-        
-        
-        
-        
-        
-        
-        <!--tạo tài khoản -->
+            
+            
             <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                       
-                        <h3 class="modal-title" style="text-align: center;font-weight: bolder;color: #A82128">CREATE AN ACCOUNT</h3>
-                    </div>
-                    <form action="#" method="post" id="frSignup">
-                        <div class="modal-body">
-                            <div class="modal-body">
-                                <div class="form-group">
-                                    <label for="CusName">Full name</label>
-                                    <input  type="text" class="form-control" name="CusName" id="CusName" required="" title="Please type your full name" placeholder="Name"  pattern="[A-Z a-z0-9]{5,30}">
-                                </div>
-                                <div class="form-group">
-                                    <label for="CusTel">Phone</label>
-                                    <input  type="text" class="form-control" name="CusTel" id="CusTel" required="" title="Please type your telephone number "  placeholder="Phone" pattern="[0-9]{10,11}">
-                                </div>
-                                <div class="form-group">
-                                    <label for="CusMail">Email</label>
-                                    <input  type="email" class="form-control" name="CusMail" id="CusMail" required="" title="Please type your email" placeholder="Email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$">
-                                </div>
-                                <div class="form-group">
-                                    <label for="CusID">Account ID</label>
-                                    <input  type="text" class="form-control" name="CusID" id="CusID" required="" title="Please type your account ID " placeholder="At least 5 characters"  pattern="[A-Za-z0-9_]{5,30}">
-                                </div>
-                                <div class="form-group">
-                                    <label for="CusPass">Password</label>
-                                    <input type="password" class="form-control" name="CusPass" id="CusPass" required="" title="Please type your password"  placeholder="At least 5 characters" pattern="[A-Za-z0-9]{5,30}">
-                                </div>
-                                <div class="form-group">
-                                    <label for="confirmPass"> Confirm password </label>
-                                    <input class="form-control" id="confirmPass" type="password" required="">
-                                    
-                                </div>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="sumbit" class="btn btn-primary" name="btnCreate">Create</button>
-                                <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-                            </div>
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">ACCOUNT INFORMATION</h3>
                         </div>
-                    </form>
+                        <div class="panel-body">
+                            <form action="#" method="post" id="frEdit">
+                                <div class="form-group">
+                                    <div class="form-group">
+                                        <label for="MemberName">Full Name</label>
+                                        <input type="text" class="form-control" name="MemberName" id="MemberName" id="MemberName" placeholder="Your name must be 5-30 characters (no numbers or special characters)"   value="" pattern="[a-z A-Z]{5,30}" required="" >
+                                    </div>
+                                    <label for="MemberID">Account ID</label>
+                                    <input type="text" class="form-control" name="MemberID" id="MemberID"  required="" value=""readonly=""  pattern="[a-z_A-Z]{5,30}">
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="MemberName">Tel</label>
+                                    <input type="text" class="form-control" name="MemberTel" id="MemberTel"  required="" value="" placeholder="Your tel number must be 10-11 digits" pattern="[0-9]{10,11}" >
+                                </div>
+                                <div class="form-group">
+                                    <label for="MemberName">Email</label>
+                                    <input type="email" class="form-control" name="MemberMail" id="MemberMail"  required="" value="" placeholder="xxx@xxx.xxx" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" >
+                                </div>
+                                <div class="form-group">
+                                    <label for="MemberName">Old Password</label>
+                                    <input type="password" class="form-control" name="OldMemberPass"  id="OldMemberPass"  required="" value="" pattern="[a-zA-Z0-9]{5,30}" >
+                                </div>
+                                <div class="form-group">
+                                    <label for="MemberName">New Password</label>
+                                    <input type="password" class="form-control" name="MemberPass"  id="MemberPass" placeholder="Your password must be 5-30 characters (no word spacing or special characters)"  required="" value="" pattern="[a-zA-Z0-9]{5,30}" >
+
+                                </div>
+                                <div class="form-group">
+                                    <label for="MemberName">Confirm Password</label>
+                                    <input type="password" class="form-control"  id="confirmMemberPass"  required="" value="" >
+                                    
+
+                                </div>
+                                <button type="submit" class="btn btn-primary btn-lg" name="btnSave">  <span class="glyphicon glyphicon-save"></span> Save changes </button>
+                                <input type="reset" class="btn btn-warning btn-lg" name="btnReset">
+                            </form>
+                        </div>
+                    </div>
                 </div>
             </div>
-        <!--footer -->
+        </div>
+            
+            
+            
+            
+            
+                 <!--footer -->
         <div class="container">
             <div class="contact-section-grids">
                  <div class="col-md-3 contact-section-grid wow fadeInLeft" data-wow-delay="0.4s">
@@ -221,28 +222,5 @@
     horizontal-align: center;
 }
         </style>
-        
-        
     </body>
 </html>
-<?php
-     if (isset($_GET["btnCreate"]) == TRUE) {
-    $cusID = $_GET["CusID"];
-    $pass = $_GET["CusPass"];
-    $cusName = $_GET["CusName"];
-    $cusEmail = $_GET["CusMail"];
-    $cusTel = $_GET["CusTel"];
-    
-    include_once './lib/connect.inc';
-    $sql = "insert into tb_customer (customer_id, customer_password, customer_name, customer_email, customer_tel) "
-            . "values('$cusID','$pass','$cusName','$cusEmail','$cusTel')";
-    
-    $result = mysqli_query($link, $sql);
-                //kiem tra ket qua thuc hien
-                if(mysqli_errno($link)){
-                    echo mysqli_error($link);
-                    exit();
-                }
-                echo "<br><h3> Create new account !!!!</h3>";
-     }
-?>
